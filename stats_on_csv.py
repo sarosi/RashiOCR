4fee601544d7b70f9578f854b857064721e76a26
GaborSarosi <gabor.sarosi@rise-world.com>
2020-03-03 12:32:22 +0100

    more labelling (now 2165), improves some images, imrpoves stats_on_csv

--- stats_on_csv.py
+++ stats_on_csv.py
@@ -10,8 +10,10 @@ import pandas as pd
 
 
 df = pd.read_csv('dataset/train/train.csv')
-df_grouped = df['Label'].value_counts()
-print(df_grouped)
+df_grouped_nominal = df['Label'].value_counts()
+df_grouped_percent = df['Label'].value_counts(normalize=True).mul(100).round(1)
+print(pd.concat([df_grouped_nominal, df_grouped_percent], axis=1, keys=['counts', '%']))
+#print(df_grouped)
 
 print(df.tail(2))
 
