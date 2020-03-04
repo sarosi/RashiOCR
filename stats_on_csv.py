c8d88bf0b0a564e74425e214c86f94bc27477bf7
GaborSarosi <gabor.sarosi@rise-world.com>
2020-03-04 17:02:05 +0100

    csv statistics now can show images of a letter from the labelled dataset

--- stats_on_csv.py
+++ stats_on_csv.py
@@ -6,14 +6,33 @@ Created on Mon Mar  2 00:22:25 2020
 @author: GaborSarosi
 """
 
-import pandas as pd
+import sys
+sys.path.append('/usr/local/lib/python3.7/site-packages')
 
+import cv2
+import pandas as pd
+from matplotlib import pyplot as plt
 
 df = pd.read_csv('dataset/train/train.csv')
-df_grouped_nominal = df['Label'].value_counts()
-df_grouped_percent = df['Label'].value_counts(normalize=True).mul(100).round(1)
-print(pd.concat([df_grouped_nominal, df_grouped_percent], axis=1, keys=['counts', '%']))
-#print(df_grouped)
 
-print(df.tail(2))
+def show_stats():
+    df_grouped_nominal = df['Label'].value_counts()
+    df_grouped_percent = df['Label'].value_counts(normalize=True).mul(100).round(1)
+    print(pd.concat([df_grouped_nominal, df_grouped_percent], axis=1, keys=['counts', '%']))
+    print(df.tail(2))
 
+def show_images_of_letter(letter):
+    rows_of_letter = df.loc[df['Label'] == letter]
+    #print(rows_of_letter)
+    #print(len(rows_of_letter))
+    for row in rows_of_letter.iterrows():
+        print(row[1][1])
+        if '.jpg' not in row[1][1]:
+            continue
+
+        image = cv2.imread('dataset/train/img/' + row[1][1], cv2.IMREAD_GRAYSCALE)
+        plt.imshow(image)
+        plt.show()
+
+
+show_images_of_letter('chaf_sofit')
